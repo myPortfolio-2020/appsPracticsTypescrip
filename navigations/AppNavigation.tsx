71e606731cbb5fb6fe17e8ddbba8b6c0a715e4f0
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-06-05 19:18:43 +0500

    po

---
+++ navigations/AppNavigation.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
+
+const AppNavigation = () => {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+    </View>
+  )
+}
+
+export default AppNavigation
