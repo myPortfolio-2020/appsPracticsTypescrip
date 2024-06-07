0c7db79cd00c60b23a5516cddc34e6b4d6f5e2ae
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-06-07 21:23:27 +0500

    asd

--- navigations/DrawerNavigation.tsx
+++ navigations/DrawerNavigation.tsx
@@ -4,13 +4,15 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import AppHome from "@/app/screens/AppHome";
 import AboutUs from "@/app/screens/AboutUs";
 import Help from "@/app/screens/Help";
+import Home from "@/app/(tabs)/Home";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="AppHome" component={AppHome} />
+      <Drawer.Screen name="Home" component={Home} />
+      {/* <Drawer.Screen name="AppHome" component={AppHome} /> */}
       <Drawer.Screen name="AboutUs" component={AboutUs} />
       <Drawer.Screen name="Help" component={Help} />
     </Drawer.Navigator>
