d78467b0e1881ca72dfaea5fa4dbddb550c151ed
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-06-07 12:52:21 +0500

    done

--- components/ViewPatient.tsx
+++ components/ViewPatient.tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const ViewPatient = () => {
   return (
     <View>
       <Text>ViewPatient</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ViewPatient
+export default ViewPatient;
